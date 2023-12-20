3516a25c0e49b72ff9752efb00fdd49ed8b53ea8
Amit Mishra <152164391+JoneySin@users.noreply.github.com>
2023-12-20 08:57:14 +0530

    Update openai.py

--- plugins/openai.py
+++ plugins/openai.py
@@ -5,14 +5,14 @@ from openai import OpenAI
 
 ai_client = OpenAI(api_key=OPENAI_API)
 
-@Client.on_message(filters.command("openai"))
+@Client.on_message(filters.command("ask"))
 async def ask_question(client, message):
     if len(OPENAI_API) == 0:
         return await message.reply("OPENAI_API is empty")
     try:
         text = message.text.split(" ", 1)[1]
     except:
-        return await message.reply_text("Command Incomplete!\nUsage: /openai your_question")
+        return await message.reply_text("Command Incomplete!\nUsage: /ask your_question")
     msg = await message.reply("Searching...")
     try:
         response = ai_client.chat.completions.create(
